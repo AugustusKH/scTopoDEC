b678e882422c22b82ab54e36cfe64816f515fc10
Pakorn Sagulkoo <pakornsagulkoo@Pakorns-MacBook-Pro.local>
2026-04-12 01:50:43 +0100

    Add __version__ to init

--- scTopoDEC/__init__.py
+++ scTopoDEC/__init__.py
@@ -2,5 +2,8 @@ import os
 # Ensure the backend is set before Keras is imported anywhere else
 os.environ['KERAS_BACKEND'] = 'tensorflow'
 
+# Add version
+__version__ = "0.1.0"
+
 # Bridge the function from api.py to the package level
 from .api import scTopoDEC
